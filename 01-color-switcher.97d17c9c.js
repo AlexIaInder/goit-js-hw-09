9beadd4fb077186baff6b569250c47e110fe480e
AlexIaInder <103319919+AlexIaInder@users.noreply.github.com>
2022-08-17 19:09:16 +0000

    Deploying to gh-pages from @ AlexIaInder/goit-js-hw-09@4f36428381b5bf88fa7226f6ccabeb6f89e52102 🚀

---
+++ 01-color-switcher.97d17c9c.js
@@ -0,0 +1,2 @@
+!function(){var t,e=document.querySelector("button[data-start]"),n=document.querySelector("button[data-stop]");n.disabled=!0,e.addEventListener("click",(function(){t=setInterval((function(){document.body.style.background="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3),e.disabled=!0,n.disabled=!1})),n.addEventListener("click",(function(){clearInterval(t),n.disabled=!0,e.disabled=!1}))}();
+//# sourceMappingURL=01-color-switcher.97d17c9c.js.map
